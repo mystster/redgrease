e01f76f9a3fad3ef81c320411a7c387afe86c36c
myst <mystster@gmail.com>
2021-05-31 13:38:16 +0000

    change mount point

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#footer')
+function removeAllChildren(parent: HTMLElement | null) {
+    if (!!parent) {
+        Array.from(parent.childNodes).forEach(x => parent.removeChild(x));
+    }
+}
+
+removeAllChildren(document.getElementById("content"));
+createApp(App).mount('#content');
